6838e6e39aaa51ceadd3141f90672edc6b4dba6b
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-11-18 10:20:59 +0100

    waiting for kibana instead of elasticseach (needs kibana index)

--- quick-start/start.sh
+++ quick-start/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Stalling for Elasticsearch"
+echo "Stalling for Kibana"
 while true; do
     sleep 1
-    status=$(curl -s --head -w %{http_code} http://0.0.0.0:9200 -o /dev/null)
+    status=$(curl -s --head -w %{http_code} http://0.0.0.0:9200/.kibana -o /dev/null)
     if [ ${status} -eq 200 ]; then
         break
     fi
