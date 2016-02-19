cc82bf45722f6bd6963af42b3bce18535be1301a
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-02-19 19:53:13 +0100

    cpu and mem per mesos slave

--- clique-zookeeper-marathon/mesos-slave.sh
+++ clique-zookeeper-marathon/mesos-slave.sh
@@ -21,4 +21,5 @@ mesos-slave --containerizers="docker,mesos" \
             --master=zk://127.0.0.1:2181/mesos \
             --work_dir=/var/run/mesos/slave$1 \
             --log_dir=/var/log/mesos/slave$1 \
-            --port=505$1
+            --port=505$1 \
+            --resources="cpus(*):2; mem(*):4096; disk(*):65536; ports(*):[31000-32000]"
