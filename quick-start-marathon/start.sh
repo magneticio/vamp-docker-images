350931557bcc43ba8d150884df1999bd10057fd8
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-02-29 14:46:54 +0100

    chronos systemd

--- quick-start-marathon/start.sh
+++ quick-start-marathon/start.sh
@@ -9,4 +9,6 @@ while true; do
     fi
 done
 
+export LANG=en_US.UTF-8
+
 java -Dvamp.gateway-driver.host=${DOCKER_HOST_IP} -Dlogback.configurationFile=/usr/local/vamp/logback.xml -Dconfig.file=/usr/local/vamp/application.conf -jar /usr/local/vamp/vamp.jar
