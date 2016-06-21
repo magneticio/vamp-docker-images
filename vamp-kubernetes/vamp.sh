705add9d6d8ba8cf294eac2fb9548ab3c762f3cc
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-06-21 22:03:02 +0200

    overriding vamp.sh

---
+++ vamp-kubernetes/vamp.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+export LANG=en_US.UTF-8
+
+# Wait for Elasticsearch and Kibana before starting Vamp.
+#while true; do
+#    sleep 1
+#    status=$(curl -s --head -w %{http_code} http://elasticsearch:9200/.kibana -o /dev/null)
+#    if [ ${status} -eq 200 ]; then
+#        break
+#    fi
+#done
+
+LOG_CONFIG=/usr/local/vamp/logback.xml
+APP_CONFIG=/usr/local/vamp/conf/application.conf
+
+if [ -e "/usr/local/vamp/conf/logback.xml" ] ; then
+    LOG_CONFIG=/usr/local/vamp/conf/logback.xml
+fi
+
+java -Dlogback.configurationFile=${LOG_CONFIG} -Dconfig.file=${APP_CONFIG} -jar /usr/local/vamp/vamp.jar
