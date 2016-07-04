a8ed87c47ae9f4c3ee93df1e8b42a38f67a4ec5e
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-07-04 22:46:27 +0200

    quick start marathon 0.9.0

--- quick-start-marathon/make.sh
+++ quick-start-marathon/make.sh
@@ -8,16 +8,24 @@ green=`tput setaf 2`
 target=$1
 mkdir -p ${target} && cd ${target}
 
-echo "${green}Cloning Vamp UI to ${target}...${reset}"
-git clone --depth=1 git@github.com:magneticio/revamp-ui.git
+#echo "${green}Cloning Vamp UI to ${target}...${reset}"
+#git clone --depth=1 git@github.com:magneticio/revamp-ui.git
+#cd ${target}/revamp-ui
+#echo "${green}Building Vamp UI...${reset}"
+#npm install -g gulp && npm install && bower install && gulp build
+#mv dist ui && tar -cvjSf ui.tar.bz2 ui
+#cd ${target}
+
+# Remove after UI is added back
+mkdir -p ${target}/revamp-ui/ui
 cd ${target}/revamp-ui
-echo "${green}Building Vamp UI...${reset}"
-npm install -g gulp && npm install && bower install && gulp build
-mv dist ui && tar -cvjSf ui.tar.bz2 ui
+echo "No 0.9.0 UI yet!" > ${target}/revamp-ui/ui/index.html
+tar -cvjSf ui.tar.bz2 ui
 cd ${target}
+#
 
 echo "${green}Cloning Vamp to ${target}...${reset}"
-git clone --depth=1 git@github.com:magneticio/vamp.git
+git clone --depth=200 git@github.com:magneticio/vamp.git
 cd ${target}/vamp
 echo "${green}Building Vamp...${reset}"
 sbt test assembly
@@ -29,6 +37,6 @@ cp -f ${target}/revamp-ui/ui.tar.bz2 ${target}/ui.tar.bz2
 rm -Rf ${target}/vamp && rm -Rf ${target}/revamp-ui
 
 cp -f ${dir}/Dockerfile ${target}/Dockerfile
-cp -fR ${dir}/conf ${target}/
-cp -f ${dir}/start.sh ${target}/
+cp -fR ${dir}/logback.xml ${dir}/application.conf ${target}/
+cp -f ${dir}/vamp.sh ${target}/
 cp -fR ${dir}/supervisord.conf ${target}/
