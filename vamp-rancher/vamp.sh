938b8d2f155c91120fa5dad691e1e6e06733cca6
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-07-05 09:29:50 +0200

    vamp 0.9.0

--- vamp-rancher/vamp.sh
+++ vamp-rancher/vamp.sh
@@ -3,13 +3,13 @@
 export LANG=en_US.UTF-8
 
 # Wait for Elasticsearch and Kibana before starting Vamp.
-#while true; do
-#    sleep 1
-#    status=$(curl -s --head -w %{http_code} http://elastic:9200/.kibana -o /dev/null)
-#    if [ ${status} -eq 200 ]; then
-#        break
-#    fi
-#done
+while true; do
+    sleep 1
+    status=$(curl -s --head -w %{http_code} http://elastic:9200/.kibana -o /dev/null)
+    if [ ${status} -eq 200 ]; then
+        break
+    fi
+done
 
 LOG_CONFIG=/usr/local/vamp/logback.xml
 APP_CONFIG=/usr/local/vamp/conf/application.conf
