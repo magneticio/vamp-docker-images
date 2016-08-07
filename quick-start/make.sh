922719cea142bdddd6a4c17f3b19e7db5b154e50
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-08-07 23:22:39 +0200

    new ui

--- quick-start/make.sh
+++ quick-start/make.sh
@@ -9,20 +9,12 @@ target=$1
 mkdir -p ${target} && cd ${target}
 
 echo "${green}Cloning Vamp UI to ${target}...${reset}"
-git clone --depth=1 git@github.com:magneticio/revamp-ui.git
-cd ${target}/revamp-ui
+git clone --depth=1 git@github.com:magneticio/vamp-ui.git
+cd ${target}/vamp-ui
 echo "${green}Building Vamp UI...${reset}"
 npm install -g gulp gulp-cli && npm install && bower install && ./setEnvironment.sh && gulp build
 mv dist ui && tar -cvjSf ui.tar.bz2 ui
 cd ${target}
-
-# Remove after UI is added back
-#mkdir -p ${target}/revamp-ui/ui
-#cd ${target}/revamp-ui
-#echo "No 0.9.0 UI yet!" > ${target}/revamp-ui/ui/index.html
-#tar -cvjSf ui.tar.bz2 ui
-#cd ${target}
-#
 
 echo "${green}Cloning Vamp to ${target}...${reset}"
 git clone --depth=200 git@github.com:magneticio/vamp.git
@@ -32,9 +24,9 @@ sbt test assembly
 
 echo "${green}Copying files...${reset}"
 cp $(find "${target}/vamp/bootstrap/target/scala-2.11" -name 'vamp-assembly-*.jar' | sort | tail -1) ${target}/vamp.jar
-cp -f ${target}/revamp-ui/ui.tar.bz2 ${target}/ui.tar.bz2
+cp -f ${target}/vamp-ui/ui.tar.bz2 ${target}/ui.tar.bz2
 
-rm -Rf ${target}/vamp && rm -Rf ${target}/revamp-ui
+rm -Rf ${target}/vamp && rm -Rf ${target}/vamp-ui
 
 cp -f ${dir}/Dockerfile ${target}/Dockerfile
 cp -fR ${dir}/logback.xml ${dir}/application.conf ${target}/
