9a48a1cf0427e30c4a62e494642e2b08248fd355
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-08-11 15:07:52 +0200

    updating vamp* images for 0.9.1

--- vamp/make.sh
+++ vamp/make.sh
@@ -8,24 +8,16 @@ green=`tput setaf 2`
 target=$1
 mkdir -p ${target} && cd ${target}
 
-#echo "${green}Cloning Vamp UI to ${target}...${reset}"
-#git clone --depth=1 git@github.com:magneticio/revamp-ui.git
-#cd ${target}/revamp-ui
-#echo "${green}Building Vamp UI...${reset}"
-#npm install -g gulp && npm install && bower install && gulp build
-#mv dist ui && tar -cvjSf ui.tar.bz2 ui
-#cd ${target}
-
-# Remove after UI is added back
-mkdir -p ${target}/revamp-ui/ui
-cd ${target}/revamp-ui
-echo "No 0.9.0 UI yet!" > ${target}/revamp-ui/ui/index.html
-tar -cvjSf ui.tar.bz2 ui
+echo "${green}Cloning Vamp UI to ${target}...${reset}"
+git clone --depth=1 https://github.com/magneticio/vamp-ui.git
+cd ${target}/vamp-ui
+echo "${green}Building Vamp UI...${reset}"
+npm install -g gulp gulp-cli && npm install && bower install && ./setEnvironment.sh && gulp build
+mv dist ui && tar -cvjSf ui.tar.bz2 ui
 cd ${target}
-#
 
 echo "${green}Cloning Vamp to ${target}...${reset}"
-git clone --depth=200 git@github.com:magneticio/vamp.git
+git clone --depth=200 https://github.com/magneticio/vamp.git
 cd ${target}/vamp
 echo "${green}Building Vamp...${reset}"
 sbt test assembly
@@ -33,6 +25,8 @@ sbt test assembly
 echo "${green}Copying files...${reset}"
 
 cp $(find "${target}/vamp/bootstrap/target/scala-2.11" -name 'vamp-assembly-*.jar' | sort | tail -1) ${target}/vamp.jar
-cp -f ${target}/revamp-ui/ui.tar.bz2 ${target}/ui.tar.bz2
+cp -f ${target}/vamp-ui/ui.tar.bz2 ${target}/ui.tar.bz2
+
+rm -Rf ${target}/vamp && rm -Rf ${target}/vamp-ui
+
 cp -f ${dir}/Dockerfile ${dir}/vamp.sh ${dir}/logback.xml ${target}/
-rm -Rf ${target}/vamp ${target}/revamp-ui
