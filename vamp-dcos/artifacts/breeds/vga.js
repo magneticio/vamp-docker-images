f54d6ea224d6f6aa3cfb985ff34589a3f80fa71e
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-09-15 13:52:25 +0200

    script update, #magneticio/vamp/755

--- vamp-dcos/artifacts/breeds/vga.js
+++ vamp-dcos/artifacts/breeds/vga.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var _ = require('lodash');
-var http = require('request');
+var _ = require('highland');
+var http = require('request-promise');
 var vamp = require('vamp-node-client');
 
 var api = new vamp.Api();
@@ -9,7 +9,7 @@ var api = new vamp.Api();
 var interval = 60; // seconds
 
 function run() {
-  api.config(function (config) {
+  api.config().each(function (config) {
 
     var mesos = config['vamp.container-driver.mesos.url'];
     var marathon = config['vamp.container-driver.marathon.url'];
@@ -17,76 +17,72 @@ function run() {
     var haproxy = config['vamp.gateway-driver.haproxy.version'];
     var logstash = config['vamp.gateway-driver.logstash.host'];
 
-    http({
-      url: mesos + '/master/slaves'
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+    _(http(mesos + '/master/slaves').promise().then(JSON.parse)).each(function (response) {
 
-          var response = JSON.parse(body);
-          var instances = response.slaves.length;
+      var instances = response.slaves.length;
 
-          var vga = {
-            "id": "/vamp/vamp-gateway-agent",
-            "args": [
-              "--storeType=zookeeper",
-              "--storeConnection=" + zookeeper,
-              "--storeKey=/vamp/haproxy/" + haproxy,
-              "--logstash=" + logstash + ":10001"
-            ],
-            "cpus": 0.2,
-            "mem": 256.0,
-            "instances": instances,
-            "container": {
-                "type": "DOCKER",
-                "docker": {
-                    "image": "magneticio/vamp-gateway-agent:0.9.1",
-                    "network": "HOST",
-                    "portMappings": [],
-                    "privileged": true,
-                    "parameters": []
-                }
-            },
-            "env": {},
-            "constraints": [
-                ["hostname", "UNIQUE"]
-            ],
-            "labels": {}
-          };
+      var vga = {
+        "id": "/vamp/vamp-gateway-agent",
+        "args": [
+          "--storeType=zookeeper",
+          "--storeConnection=" + zookeeper,
+          "--storeKey=/vamp/haproxy/" + haproxy,
+          "--logstash=" + logstash + ":10001"
+        ],
+        "cpus": 0.2,
+        "mem": 256.0,
+        "instances": instances,
+        "acceptedResourceRoles": [
+          "slave_public",
+          "*"
+        ],
+        "container": {
+          "type": "DOCKER",
+          "docker": {
+            "image": "magneticio/vamp-gateway-agent:0.9.1",
+            "network": "HOST",
+            "portMappings": [],
+            "privileged": true,
+            "parameters": []
+          }
+        },
+        "env": {},
+        "constraints": [
+          ["hostname", "UNIQUE"]
+        ],
+        "labels": {}
+      };
 
-          console.log('checking if deployed: /vamp/vamp-gateway-agent');
+      console.log('checking if deployed: /vamp/vamp-gateway-agent');
 
-          http({
-            url: marathon + '/v2/apps/vamp/vamp-gateway-agent',
-            }, function (error, response, body) {
+      _(http(marathon + '/v2/apps/vamp/vamp-gateway-agent').promise().then(JSON.parse).catch(function () {
+        return null;
+      })).each(function (app) {
 
-              var deploy = response.statusCode == 404;
+        if (app) {
+          console.log('already deployed, checking number of instances...');
+          console.log('deployed instances: ' + app.app.instances);
+          console.log('expected instances: ' + instances);
 
-              if (!deploy) console.log('already deployed, checking number of instances...');
-
-              if (!deploy && response.statusCode == 200) {
-                var app = JSON.parse(body)
-                console.log('deployed instances: ' + app.app.instances);
-                console.log('expected instances: ' + instances);
-                deploy = app.app.instances != instances;
-              }
-
-              if (deploy) {
-
-                console.log('deploying...');
-
-                http({
-                  url: marathon + '/v2/apps',
-                  method: 'POST',
-                  json: vga
-                  }, function (error, response, body) {
-                    if (!error && response.statusCode === 201)
-                      console.log('done.');
-                    else
-                      console.log('error.');
-                });
-              } else console.log('done.');
-          });
+          if (app.app.instances == instances) {
+            console.log('done.');
+            return;
+          }
         }
+
+        console.log('deploying...');
+
+        http({
+          url: marathon + '/v2/apps',
+          method: 'POST',
+          json: vga
+        }).promise().then(function () {
+          console.log('done.');
+        }).catch(function (response) {
+          console.log('error - ' + response.statusCode);
+          return null;
+        })
+      });
     });
   });
 }
