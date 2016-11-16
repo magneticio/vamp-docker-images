e69df33329b811dc84ee6fdab8a3de0a66ff7f08
Mikael Sörlin <mikael@magnetic.io>
2016-11-16 19:13:43 +0100

    Fail on missing 'npm' and install modules locally

--- vamp/make.sh
+++ vamp/make.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
+set -o errexit # Abort script at first error (command exits non-zero).
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 reset=`tput sgr0`
 green=`tput setaf 2`
+
+# Exit if we don't have npm available
+hash npm || exit 1
 
 target=$1
 mkdir -p ${target} && cd ${target}
@@ -14,7 +17,13 @@ echo "${green}Cloning Vamp UI to ${target}...${reset}"
 git clone --depth=200 https://github.com/magneticio/vamp-ui.git
 cd ${target}/vamp-ui
 echo "${green}Building Vamp UI...${reset}"
-npm install -g "gulpjs/gulp#4.0" && npm install && bower install && ./environment.sh && gulp build
+
+npm install "gulpjs/gulp#4.0" bower \
+  && npm install \
+  && ./node_modules/.bin/bower install \
+  && ./environment.sh \
+  && ./node_modules/.bin/gulp build
+
 mv dist ui && tar -cvjSf ui.tar.bz2 ui
 cd ${target}
 
