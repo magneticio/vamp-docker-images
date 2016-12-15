72e88ff7c852e429769315706fbf0fd5b95b504a
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-12-15 10:02:15 +0100

    fixing paths

--- vamp-custom/make.sh
+++ vamp-custom/make.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -o errexit # Abort script at first error (command exits non-zero).
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+target=$1
+mkdir -p ${target} && cd ${target}
+
 cp -fR ${dir}/../artifacts ${target}/artifacts
 cp -f ${dir}/Dockerfile ${target}/Dockerfile
 cp -f ${dir}/vamp.sh ${target}/vamp.sh
