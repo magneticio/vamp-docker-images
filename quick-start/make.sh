72cf6a00611367ad5feaa8413c17e27a4384e2ef
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-01-15 18:54:45 +0100

    moving artifacts to vamp-artifact repo

--- quick-start/make.sh
+++ quick-start/make.sh
@@ -40,7 +40,21 @@ cp -f ${target}/vamp-ui/ui.tar.bz2 ${target}/ui.tar.bz2
 
 rm -Rf ${target}/vamp && rm -Rf ${target}/vamp-ui
 
-cp -fR ${dir}/../artifacts ${target}/artifacts
+mkdir -p ${target}/artifacts/breeds ${target}/artifacts/blueprints ${target}/artifacts/workflows
+
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/breeds/allocation.js -o ${target}/artifacts/breeds/allocation.js
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/breeds/health.js -o ${target}/artifacts/breeds/health.js
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/breeds/kibana.js -o ${target}/artifacts/breeds/kibana.js
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/breeds/metrics.js -o ${target}/artifacts/breeds/metrics.js
+
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/blueprints/sava:1.0.yml -o ${target}/artifacts/blueprints/sava:1.0.yml
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/blueprints/sava:1.1.yml -o ${target}/artifacts/blueprints/sava:1.1.yml
+
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/workflows/allocation.yml -o ${target}/artifacts/workflows/allocation.yml
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/workflows/health.yml -o ${target}/artifacts/workflows/health.yml
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/workflows/kibana.yml -o ${target}/artifacts/workflows/kibana.yml
+curl https://raw.githubusercontent.com/magneticio/vamp-artifacts/master/workflows/metrics.yml -o ${target}/artifacts/workflows/metrics.yml
+
 cp -f ${dir}/Dockerfile ${target}/Dockerfile
 cp -fR ${dir}/logback.xml ${dir}/application.conf ${target}/
 cp -f ${dir}/vamp.sh ${target}/
