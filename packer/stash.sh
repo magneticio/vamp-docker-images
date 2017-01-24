3e618a18ea8a8cb14e0731ca2983f944879e52e9
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-01-24 10:34:57 +0100

    packer docker image

---
+++ packer/stash.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+project=$1
+
+if [[ -z ${project} ]]; then
+  echo "no project name provided"
+fi
+
+version=$2
+
+if [[ -z ${version} ]]; then
+  echo "no version name provided"
+fi
+
+echo "project: ${project}"
+echo "version: ${version}"
+
+if [ ! "$(ls -A /usr/local/src)" ]; then
+  echo "nothing to be stashed"
+  exit 0
+fi
+
+target="/usr/local/stash/${project}-${version}"
+katana="/usr/local/stash/${project}-katana"
+
+echo "stashing /usr/local/src as ${target}"
+
+rm -Rf ${target} && mkdir -p ${target}
+cp -a "/usr/local/src/." "${target}/"
+rm -Rf ${katana} && ln -s ${target} ${katana}
