53f1df505ba0e48c686c7559405a8f2bf4140e24
Mikael Sörlin <mikael@magnetic.io>
2017-02-10 14:37:10 +0100

    Add tag-release.sh script

---
+++ tag-release.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -o errexit # Abort script at first error (command exits non-zero).
+
+root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+reset=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+
+: "${PUSH:=false}"
+
+if [[ -z $1 ]] ; then
+  >&2 echo "Missing argument!"
+  echo "Usage:"
+  echo "  tag-release.sh <version>"
+  echo ""
+  echo "Example:"
+  echo "  tag-release.sh 0.9.3"
+  exit 1
+else
+  TAG="$1"
+  if [[ $2 = push ]] ; then
+    PUSH="true"
+  fi
+fi
+
+if [[ -n $(git status --porcelain 2> /dev/null) ]] ; then
+  echo "${red}Warning!${reset} You got untracked changes, exiting!"
+  exit 1
+fi
+
+echo "${green}
+vamp tagger
+                                                                          by magnetic.io
+${reset}"
+
+# Tag our current repository as well
+git tag "$TAG"
+
+workspace=${root}/target
+mkdir -p ${workspace}
+
+tag() {
+  project=$1
+  url="git@github.com:magneticio/${project}.git"
+  echo "${green}project: ${yellow}${project}${reset}"
+  cd ${workspace}
+
+  if [[ -d ${workspace}/${project} ]] ; then
+    echo "${green}updating existing repository${reset}"
+
+    cd ${workspace}/${project}
+
+    git reset --hard
+    git checkout master
+    git pull
+
+  else
+    git clone --depth=200 "$url"
+    cd ${workspace}/${project}
+
+  fi
+
+  if [[ $( git describe --tags ) != "$TAG" ]] ; then
+    git tag "$TAG"
+  fi
+
+  if [[ "$PUSH" = "true" ]] ; then
+    git push --tags
+  fi
+}
+
+tag vamp
+tag vamp-haproxy
+tag vamp-elasticsearch
+tag vamp-zookeeper
+tag vamp-etcd
+tag vamp-redis
+tag vamp-consul
+tag vamp-dcos
+tag vamp-docker
+tag vamp-kubernetes
+tag vamp-rancher
+tag vamp-lifter
+tag vamp-ui
+tag vamp-artifacts
+tag vamp-runner
+tag vamp-gateway-agent
+tag vamp-workflow-agent
