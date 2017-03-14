b308013087d81493963d619ceb0f9424ec6ef049
Mikael Sörlin <mikael@magnetic.io>
2017-03-14 15:43:32 +0100

    Use buildserver instead of local leiningen

--- clique-zookeeper/make.sh
+++ clique-zookeeper/make.sh
@@ -14,7 +14,18 @@ mkdir -p ${target} && cd ${target}
 echo "${green}building: ${yellow}zk-web${reset}"
 git clone --depth=1 https://github.com/qiuxiafei/zk-web.git
 cd ${target}/zk-web
-lein uberjar
+
+build_server="magneticio/buildserver"
+docker pull $build_server
+docker run \
+  --interactive \
+  --tty \
+  --rm \
+  --volume ${target}/zk-web:/srv/src \
+  --workdir=/srv/src \
+  $build_server \
+    lein uberjar
+
 cp $(find "${target}/zk-web/target" -name 'zk-web-*-standalone.jar' | sort | tail -1) ${target}/zk-web.jar
 
 echo "${green}copying files...${reset}"
