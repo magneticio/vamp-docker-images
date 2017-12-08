d57135fa70f514e43fd2222d037f2a213576557b
Lucian Gabor <lucian.gabor@busymachines.com>
2017-12-08 19:04:23 +0200

    fix image pushing

--- tests/docker/push.sh
+++ tests/docker/push.sh
@@ -44,36 +44,10 @@ docker_images="
 
 # Check that we have our images available
 for i in $docker_images; do
-  declare -i is_custom=$(docker images --format "{{.Repository}}:{{.Tag}}" "$i:*" | grep -Ei ':[a-z].+' | wc -l)
-  if [ $is_custom -gt 0 ]; then
-    for j in $(docker images --format "{{.Repository}}:{{.Tag}}" "$i:*" | grep -E ":$TAG"); do
-      if [ "$TAG" = "master" ]; then
-        image="${j/master/katana}"
-        echo "${green}Pushing image: ${image}${reset}"
-        docker tag "$j" "$image"
-        docker push "$image"
-      else
-        echo "${green}Pushing image: ${j}${reset}"
-        docker push "$j"
-      fi
-    done
-  else
-    if [ "$TAG" = "master" ]; then
-      declare -i is_release=$(docker images --format "{{.Repository}}:{{.Tag}}" "$i:*" | grep -E ':[0-9]\.' | wc -l)
-      if [ $is_release -gt 0 ]; then
-        for j in $(docker images --format "{{.Repository}}:{{.Tag}}" "$i:*" | grep -E ':[0-9]\.'); do
-          image="${j/[0-9]\.[0-9]\.[0-9]/katana}"
-          echo "${green}Pushing image: ${image}${reset}"
-          docker tag "$j" "$image"
-          docker push "$image"
-        done
-      else
-        >&2 echo "${red}Error: No such image: ${i}${reset}"
-        >&2 echo "Exiting..."
-        exit 1
-      fi
-    fi
-  fi
+  for j in $(docker images --format "{{.Repository}}:{{.Tag}}" "$i:$TAG*"); do
+    echo "${green}Pushing image: ${j}${reset}"
+    docker push "$j"
+  done
 done
 
 sed -i -e "s/\(\"image\": \"magneticio\/vamp:\).*\(-dcos\",\)/\1$TAG\2/g" ../dcos/marathon/vamp.json
