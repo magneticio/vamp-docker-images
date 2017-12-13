3012a9abff82637a4b6e0ae421312fd710832152
Lucian Gabor <lucian.gabor@busymachines.com>
2017-12-13 14:19:26 +0200

    enable fully custom image tags

--- compose/build.sh
+++ compose/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ${dir}/common.sh
+source ${dir}/common.sh "${@}"
 
 if [ "${CLEAN_BUILD:-true}" = "true" ]; then
   echo "${green}Building ${yellow}'vamp-gateway-agent' and 'vamp-workflow-agent'${reset} Docker images${reset}"
@@ -12,7 +12,7 @@ echo "${green}Building ${yellow}'vamp'${reset} Docker image${reset}"
 build-vamp
 
 echo "${green}Building ${yellow}'vamp-compose'${reset} Docker image${reset}"
-cd ${dir} && ../build.sh --build --image=vamp-compose
+cd ${dir} && ../build.sh --build --version=${vamp_version} --image=vamp-compose
 
 cat > .env <<EOF
 VAMP_VERSION=${vamp_version}
