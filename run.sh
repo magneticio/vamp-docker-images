9650ff033eb6cead46dd483d3b62eef0a3504552
Lucian Gabor <lucian.gabor@busymachines.com>
2017-12-13 17:51:01 +0200

    run vga independently of all cliques, except for the quick start

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ green=`tput setaf 2`
 yellow=`tput setaf 3`
 
 flag_help=0
+flag_vga=0
 flag_clique_etcd=0
 flag_clique_consul=0
 flag_clique_zookeeper=0
@@ -18,38 +19,26 @@ flag_quick_start=0
 
 for key in "$@"
 do
-case ${key} in
+case ${key/\//} in
     -h|--help)
     flag_help=1
     ;;
-    clique-etcd)
-    flag_clique_etcd=1
+    vga|vamp-gatewa-agent)
+    flag_vga=1
     ;;
-    clique-etcd/)
+    clique-etcd)
     flag_clique_etcd=1
     ;;
     clique-consul)
     flag_clique_consul=1
     ;;
-    clique-consul/)
-    flag_clique_consul=1
-    ;;
     clique-zookeeper)
-    flag_clique_zookeeper=1
-    ;;
-    clique-zookeeper/)
     flag_clique_zookeeper=1
     ;;
     clique-zookeeper-marathon)
     flag_clique_zookeeper_marathon=1
     ;;
-    clique-zookeeper-marathon/)
-    flag_clique_zookeeper_marathon=1
-    ;;
     quick-start)
-    flag_quick_start=1
-    ;;
-    quick-start/)
     flag_quick_start=1
     ;;
     -v=*|--version=*)
@@ -74,7 +63,7 @@ ${reset}"
 
 error=0
 
-if [[ ${flag_help} -eq 0 && ${flag_clique_etcd} -eq 0 && ${flag_clique_consul} -eq 0 && ${flag_clique_zookeeper} -eq 0 && ${flag_clique_zookeeper_marathon} -eq 0 && ${flag_quick_start} -eq 0 ]]; then
+if [[ ${flag_help} -eq 0&& ${flag_vga} -eq 0 && ${flag_clique_etcd} -eq 0 && ${flag_clique_consul} -eq 0 && ${flag_clique_zookeeper} -eq 0 && ${flag_clique_zookeeper_marathon} -eq 0 && ${flag_quick_start} -eq 0 ]]; then
     error=1
     echo "${red}No task specified.${reset}"
     echo
@@ -88,10 +77,11 @@ if [[ ${task_count} -gt 1 ]]; then
 fi
 
 if [ ${flag_help} -eq 1 ] || [[ ${error} -ne 0 ]]; then
-    echo "${green}Usage: $0 clique-*|quick-start [options] ${reset}"
-    echo "${yellow}  clique-etcd                ${green}Run HAProxy, etcd, Elasticsearch, Logstash, Kibana and Vamp Gateway Agent.${reset}"
-    echo "${yellow}  clique-consul              ${green}Run HAProxy, Consul, Elasticsearch, Logstash, Kibana and Vamp Gateway Agent.${reset}"
-    echo "${yellow}  clique-zookeeper           ${green}Run HAProxy, ZooKeeper, Elasticsearch, Logstash, Kibana and Vamp Gateway Agent.${reset}"
+    echo "${green}Usage: $0 vga|clique-*|quick-start [options] ${reset}"
+    echo "${yellow}  vga (vamp-gateway-agent)   ${green}Run Vamp Gateway Agent.${reset}"
+    echo "${yellow}  clique-etcd                ${green}Run HAProxy, etcd, Elasticsearch, Logstash and Kibana.${reset}"
+    echo "${yellow}  clique-consul              ${green}Run HAProxy, Consul, Elasticsearch, Logstash and Kibana.${reset}"
+    echo "${yellow}  clique-zookeeper           ${green}Run HAProxy, ZooKeeper, Elasticsearch, Logstash and Kibana.${reset}"
     echo "${yellow}  clique-zookeeper-marathon  ${green}Run all from 'clique-zookeeper' and Mesos, Marathon and Chronos.${reset}"
     echo "${yellow}  quick-start                ${green}Vamp quick start with Marathon.${reset}"
     echo "${yellow}  -h  |--help                ${green}Help.${reset}"
@@ -102,52 +92,44 @@ if [ ${flag_help} -eq 1 ] || [[ ${error} -ne 0 ]]; then
     fi
 fi
 
-target_dir=${dir}/"target"
-compose_file=${target_dir}/compose.yml
-
-
-function command_exists() {
-	command -v "$@" > /dev/null 2>&1
-}
-
-# Return the first IP address in the bridge network by incrementing the 4th
-# octet of the gateway IP
-function get_docker_host_ip() {
-  case "$( uname -s )" in
-    Linux)
-      hostname -I | awk '{ print $1}'
-      # docker network inspect bridge \
-      #   --format "{{ (index .IPAM.Config 0).Gateway  }}" \
-      #     | awk -F'.' '{ print $1 "." $2 "." $3 "." ( $4 += 1 ) }'
-      ;;
-    Darwin)
-      echo "192.168.65.2" # docker-machine ip default
-      ;;
-  esac
-}
+if [[ ${flag_vga} -eq 1 ]]; then
+    echo "${green}Running: vamp-gateway-agent${reset}"
+    docker run -d --name=vamp_vga \
+               --net=host \
+               --privileged \
+               -e VAMP_GATEWAY_AGENT_LOGO=0 \
+               -e VAMP_KEY_VALUE_STORE_TYPE=zookeeper \
+               -e VAMP_KEY_VALUE_STORE_CONNECTION=127.0.0.1:2181 \
+               -e VAMP_KEY_VALUE_STORE_PATH=/vamp/vamp/gateways/haproxy/1.7/configuration \
+               -e VAMP_ELASTICSEARCH_URL=http://127.0.0.1:9200 \
+               magneticio/vamp-gateway-agent:${vamp_version}
+fi
 
 if [[ ${flag_clique_etcd} -eq 1 ]]; then
     echo "${green}Running: clique-etcd${reset}"
-    docker run --net=host \
+    docker run -d --name=vamp_clique-etcd \
+               --net=host \
                --security-opt=seccomp:unconfined \
                -v /var/run/docker.sock:/var/run/docker.sock \
-               -v "/sys/fs/cgroup:/sys/fs/cgroup" \
+               -v /sys/fs/cgroup:/sys/fs/cgroup \
                magneticio/vamp-clique-etcd:${vamp_version}
 fi
 
 if [[ ${flag_clique_consul} -eq 1 ]]; then
     echo "${green}Running: clique-consul${reset}"
-    docker run --net=host \
+    docker run -d --name=vamp_clique-consul \
+               --net=host \
                --security-opt=seccomp:unconfined \
                -v /var/run/docker.sock:/var/run/docker.sock \
-               -v "/sys/fs/cgroup:/sys/fs/cgroup" \
+               -v /sys/fs/cgroup:/sys/fs/cgroup \
                magneticio/vamp-clique-consul:${vamp_version}
 fi
 
 if [[ ${flag_clique_zookeeper} -eq 1 ]]; then
     echo "${green}Running: clique-zookeeper${reset}"
-    docker run -v /var/run/docker.sock:/var/run/docker.sock \
-               -v "/sys/fs/cgroup:/sys/fs/cgroup" \
+    docker run -d --name=vamp_clique-zookeeper \
+               -v /var/run/docker.sock:/var/run/docker.sock \
+               -v /sys/fs/cgroup:/sys/fs/cgroup \
                -p 5050:5050 \
                -p 8989:8989 \
                -p 9200:9200 \
@@ -158,12 +140,13 @@ fi
 if [[ ${flag_clique_zookeeper_marathon} -eq 1 ]]; then
     echo "${green}Running: clique-zookeeper-marathon${reset}"
 
-    DOCKER_HOST_IP="$( get_docker_host_ip )"
-
-    docker run -v /var/run/docker.sock:/var/run/docker.sock \
-           -v "/sys/fs/cgroup:/sys/fs/cgroup" \
-           -e "DOCKER_HOST_IP=${DOCKER_HOST_IP}" \
+    docker run -d --name=vamp_clique-zookeeper-marathon \
+           -v /var/run/docker.sock:/var/run/docker.sock \
+           -v /sys/fs/cgroup:/sys/fs/cgroup \
+           -e DOCKER_HOST_IP=172.17.0.1 \
            -p 5050:5050 \
+           -p 5051:5051 \
+           -p 5052:5052 \
            -p 9090:9090 \
            -p 8989:8989 \
            -p 4400:4400 \
@@ -176,14 +159,15 @@ fi
 if [[ ${flag_quick_start} -eq 1 ]]; then
     echo "${green}Running: quick-start${reset}"
 
-    DOCKER_HOST_IP="$( get_docker_host_ip )"
-
-    docker run -v /var/run/docker.sock:/var/run/docker.sock \
-           -v "/sys/fs/cgroup:/sys/fs/cgroup" \
-           -e "DOCKER_HOST_IP=${DOCKER_HOST_IP}" \
+    docker run -d --name=vamp_quick-start \
+           -v /var/run/docker.sock:/var/run/docker.sock \
+           -v /sys/fs/cgroup:/sys/fs/cgroup \
+           -e DOCKER_HOST_IP=172.17.0.1 \
            -p 8080:8080 \
            -p 8081:8081 \
            -p 5050:5050 \
+           -p 5051:5052 \
+           -p 5052:5052 \
            -p 9090:9090 \
            -p 8989:8989 \
            -p 4400:4400 \
